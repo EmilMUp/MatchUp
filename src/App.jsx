5e9ace9537ac195e51062901a13b484862fa4fc7
EmilMUp <emil.mup@gmail.com>
2025-01-05 19:59:12 +0100

    added navbar and css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import './index.css';  // Importing the global CSS file
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+    </>
+  
+  )
+}
+
+export default App
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <>
+     <h1 className="nav">navbar</h1>   
+    </>
+  )
+}
+
+export default Navbar;
